6d1d412b243d6bcf2aa2e7cf66a78c2162928afe
Stanislav Pyatnicyn <pyatnicyn.stanislav@gmail.com>
2022-12-22 23:40:46 +0300

    Updated eslint config

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -31,14 +31,14 @@ export class UserController {
   @ApiOperation({ summary: 'Find all users' })
   @ApiResponse({ type: ReturnUserDto, isArray: true })
   @Get()
-  findAll(@Query() pagination: PaginationDto): Promise<PaginationResponse<ReturnUserDto[]>> {
+  public findAll(@Query() pagination: PaginationDto): Promise<PaginationResponse<ReturnUserDto[]>> {
     return this.userService.findAll(pagination);
   }
 
   @ApiOperation({ summary: 'Find user by id' })
   @ApiResponse({ type: ReturnUserDto })
   @Get(':id')
-  findOne(@Param('id') id: string): Promise<ReturnUserDto> {
+  public findOne(@Param('id') id: string): Promise<ReturnUserDto> {
     return this.userService.findById(id);
   }
 
@@ -46,14 +46,14 @@ export class UserController {
   @ApiBody({ type: UpdateUserDto })
   @ApiResponse({ type: ApiUpdateResultDto })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<UpdateResult> {
+  public update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<UpdateResult> {
     return this.userService.update(id, updateUserDto);
   }
 
   @ApiOperation({ summary: 'Delete user by id' })
   @ApiResponse({ type: ApiDeleteResultDto })
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<DeleteResult> {
+  public remove(@Param('id') id: string): Promise<DeleteResult> {
     return this.userService.remove(id);
   }
 }
